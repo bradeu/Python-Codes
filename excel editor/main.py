828ccae533f9acb7ade1c0f1cf64039f0b4f6905
Bradley <Bradley@Brads-MacBook-Air.local>
2023-10-19 18:38:00 -0700

    Add excel editor

---
+++ excel editor/main.py
@@ -0,0 +1,35 @@
+import openpyxl as xl
+from openpyxl.chart import BarChart, Reference
+
+
+def main():
+    process_excel("transactions.xlsx")
+
+
+def process_excel(filename):
+    wb = xl.load_workbook(filename)
+    sheet = wb["Sheet1"]
+
+    for row in range(2, sheet.max_row + 1):
+        # column 3 iterate
+        cell = sheet.cell(row, 3)
+        corrected_price = cell.value * 0.9
+        # 10% discount
+        corrected_price_cell = sheet.cell(row, 4)
+        corrected_price_cell.value = corrected_price
+
+    values = Reference(sheet,
+             min_row=2,
+             max_row=sheet.max_row,
+             min_col=4,
+             max_col=4
+              )
+
+    chart = BarChart()
+    chart.add_data(values)
+    sheet.add_chart(chart, "a5")
+
+    wb.save(filename)
+
+
+main()
